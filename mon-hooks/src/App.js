684e126a32a8062248b6400d69ab523923d67b6a
walid <wilwil1515@hotmail.com>
2021-01-04 13:33:56 +0200

    first-commit

---
+++ mon-hooks/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+import { moviesData } from './Components/MoviesData';
+import SearchMovie from './Components/SearchMovie/SearchMovie';
+import MoviesList from './Components/MoviesList';
+import AddMovie from './Components/AddMovie/AddMovie';
+
+import './App.css';
+
+
+function App() {
+  const [moviesList, setMoviesList] = useState(moviesData);
+  const [nameSearch, setNameSearch] = useState('');
+  const [ratingSearch, setRatingSearch] = useState(1);
+
+  const addNewMovie = (e, newMovie) => {
+    e.preventDefault();
+    setMoviesList([...moviesList, newMovie]);
+  };
+
+  return (
+    <div className="App">
+      <SearchMovie
+        setNameSearch={setNameSearch}
+        ratingSearch={ratingSearch}
+        setRatingSearch={setRatingSearch}
+      />
+      {/* <MoviesList
+        moviesList={moviesList}
+        nameSearch={nameSearch}
+        ratingSearch={ratingSearch}
+      /> */}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <AddMovie addNewMovie={addNewMovie} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ mon-hooks/src/Components/MoviesData.js
@@ -0,0 +1,70 @@
+export const moviesData = [
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19589780&cfilm=283794.html',
+      rating: 4,
+      name: "Nos plus Belles Années",
+      date: '2014',
+      type: 'Action | Crime',
+      description:
+        "A relentlessly-paced hybrid of gritty crime thriller and Lovecraftian supernatural horror, The Devil's Mile follows a trio of kidnappers who take an ill-advised detour en route to deliver...",
+    },
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19586267&cfilm=199293.html',
+      rating: 5,
+      name: 'sous les étoiles de paris',
+      date: '1968',
+      type: 'Western',
+      description:
+        'A mysterious stranger with a harmonica joins forces with a notorious desperado to protect a beautiful widow from a ruthless assassin working for the railroad.',
+    },
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19589594&cfilm=256588.html',
+      rating: 4,
+      name: 'Wonder woman 1984',
+      date: '1976',
+      type: 'Crime | Drama',
+      description:
+        'A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.',
+    },
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19590528&cfilm=251354.html',
+      rating: 4,
+      name: 'soul',
+      date: '1979',
+      type: 'Action | Adventure',
+      description:
+        'In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang.',
+    },
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19590855&cfilm=275220.html',
+      rating: 3,
+      name: 'Minute DAns luniver',
+      date: '2006',
+      type: 'Comedy | Drama',
+      description:
+        'A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus.',
+    },
+    {
+      id: Math.random(),
+      image:
+        'https://www.allocine.fr/video/player_gen_cmedia=19590814&cfilm=276644.html',
+      rating: 5,
+      name: 'Cest nous, les héros Bande-annonce',
+      date: '2003',
+      type: 'Action',
+      description:
+        "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.'The Bride' decided to escape her life as a killer.",
+      details: 'https://www.allocine.fr/video/player_gen_cmedia=19590814&cfilm=276644.html'
+
+    },
+    ];
